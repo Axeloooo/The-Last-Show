cfd811189d8330b0e74061172c4664136fd07a1c
Axeloooo <axelshz@gmail.com>
2023-04-07 18:38:58 -0600

    requirements.txt files added

--- functions/create-obituary/main.py
+++ functions/create-obituary/main.py
@@ -1,1 +1,4 @@
 # add your create-obituary function here
+
+def lambda_handler(event, context):
+    print("hello world!")
--- functions/get-obituaries/main.py
+++ functions/get-obituaries/main.py
@@ -1,1 +1,4 @@
 # add your get-obituaries function here
+
+def lambda_handler(event, context):
+    print("hello world!")
