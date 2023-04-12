0bcb8d03b4aef464eb2a14b1f5e7070b1de1a018
podgaietska <podgaietska.m@gmail.com>
2023-04-11 18:06:15 -0600

    select img option added

--- src/components/ObituaryCreator.js
+++ src/components/ObituaryCreator.js
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function ObituaryCreator({changeVisibilityCreator, onAddObituary}) {
     const [name, setName] = useState("");
     const dt = new Date();
     dt.setMinutes(dt.getMinutes() - dt.getTimezoneOffset());
-    const[date, setDate] = useState(dt.toISOString().slice(0, 16));
+    const [date, setDate] = useState(dt.toISOString().slice(0, 16));
+    const [img, setImg] = useState(null);
 
 const WriteObituary = (name) => {
     onAddObituary(name);
@@ -15,8 +16,10 @@ const WriteObituary = (name) => {
         <div className="obituary-creator-container">
             <div className="obituary-creator-close-button" onClick={changeVisibilityCreator}>x</div>
             <h1 className="title">Create a New Obituary</h1>
-            <img src="https://www.seekpng.com/png/detail/154-1541124_graphics-for-scroll-clip-art-graphics-fancy-design.png" alt="obituary" />
-            <p>Select an image for the deceased</p>
+            <label htmlFor="img-upload" className="img-upload-label">
+                <input id="img-upload" type="file" onChange={(e) => setImg(e.target.files[0])}/>
+                {img ? (<p>Select an image for the deceased ({img.name})</p>) : (<p>Select an image for the deceased</p>)}
+            </label>
             <input className="creator-name-input" type="text" placeholder="Name of the deceased" value={name} onChange={(e) => setName(e.target.value)}/>
             <div className="creator-date-container">
                 <p>Born: </p>
