2ffbb7830590e118f9fb43344800f48a002fbd17
podgaietska <podgaietska.m@gmail.com>
2023-04-20 21:56:17 -0600

    validate file is present added

--- src/components/ObituaryCreator.js
+++ src/components/ObituaryCreator.js
@@ -10,7 +10,7 @@ function ObituaryCreator({changeVisibilityCreator, onAddObituary}) {
 
     const onSubmit = (e) => {
         e.preventDefault();
-        WriteObituary(name, birthDate, deathDate);
+        WriteObituary(name, birthDate, deathDate, );
     }
 
     const WriteObituary = (name, birthDate, deathDate) => {
@@ -18,13 +18,18 @@ function ObituaryCreator({changeVisibilityCreator, onAddObituary}) {
         changeVisibilityCreator();
     };
 
+    const addImg = (e) => {
+        console.log(e.target.files[0].name);
+        setImg(e.target.files[0].name);
+    };
+
     return (
         <form className="obituary-creator-container" onSubmit={onSubmit}>
             <div className="obituary-creator-close-button" onClick={changeVisibilityCreator}>x</div>
             <h1 className="title">Create a New Obituary</h1>
             <label htmlFor="img-upload" className="img-upload-label">
-                <input id="img-upload" type="file" onChange={(e) => setImg(e.target.files[0])}/>
-                {img ? (<p>Select an image for the deceased ({img.name})</p>) : (<p>Select an image for the deceased</p>)}
+                <input id="img-upload" type="file" onChange={(e) => addImg(e)} required/>
+                {img ? (<p>Select an image for the deceased ({img})</p>) : (<p>Select an image for the deceased</p>)}
             </label>
             <input className="creator-name-input" type="text" placeholder="Name of the deceased" value={name} onChange={(e) => setName(e.target.value)} required/>
             <div className="creator-date-container">
