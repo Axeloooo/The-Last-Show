665494fcb2f709d57d7a758af86b6b1b0e0d4ff6
podgaietska <podgaietska.m@gmail.com>
2023-04-21 13:35:48 -0600

    Audio element added

--- src/components/Obituary.js
+++ src/components/Obituary.js
@@ -1,7 +1,29 @@
+import React, { useState, useMemo, useEffect } from "react";
+
 function Obituary({onUpdateObituary, open, name, born_year, died_year, obituary_text, obituary_image_url, obituary_audio_url, formatDate}) {
+    const [playing, setPlaying] = useState(false);
+    const [audio, setAudio] = useState(new Audio(obituary_audio_url));
+
+
+    const toggleSound = () => {
+        setPlaying(!playing);
+    };
+
+    useEffect(() => {
+        playing ? audio.play() : audio.pause();
+    }, [playing]);
+
+    useEffect(() => {
+        audio.addEventListener("ended", () => setPlaying(false));
+        return () => {
+            audio.removeEventListener("ended", () => setPlaying(false));
+        };
+    }, []);
     
+
     const changeObituaryVisibility = () => {
         open = !open;
+        setAudio(new Audio(obituary_audio_url));
         onUpdateObituary({
             name: name,
             born_year: born_year,
@@ -15,7 +37,8 @@ function Obituary({onUpdateObituary, open, name, born_year, died_year, obituary_
 
 
     return (
-        <div className="obituary" onClick={changeObituaryVisibility}>
+        <div className="obituary">
+            <div className="obituary-container" onClick={changeObituaryVisibility}>
             <div className="obituary-img" >
                 <img src={obituary_image_url} alt="obituary"/>
                 <div className="obituary-img-overlay"></div>
@@ -28,10 +51,13 @@ function Obituary({onUpdateObituary, open, name, born_year, died_year, obituary_
             <div className="obituary-body">
                 <p>{obituary_text}</p>
             </div>
-            ) }
-            <div className="obituary-buttons">
-                <button className="obituary-button-play">Play</button>
+            )}
             </div>
+            {open && (
+            <div className="obituary-buttons">
+                <button className="obituary-button-play" onClick={() => toggleSound()}>Play</button>
+            </div>
+            )}
         </div>
     );
 }
