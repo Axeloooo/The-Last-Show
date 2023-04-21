d5e00882d6fc81f9a79f0e9c50b1e8c342194409
Axeloooo <axelshz@gmail.com>
2023-04-20 22:39:26 -0600

    Frontend and backend connected for create obituary

--- src/App.js
+++ src/App.js
@@ -1,27 +1,49 @@
-import './App.css';
-import Header from './components/Header';
-import ObituaryList from './components/ObituaryList';
-import ObituaryCreator from './components/ObituaryCreator';
-import { useState, useEffect } from 'react';
+import "./App.css";
+import Header from "./components/Header";
+import ObituaryList from "./components/ObituaryList";
+import ObituaryCreator from "./components/ObituaryCreator";
+import { useState, useEffect } from "react";
 
 function App() {
   const [showCreator, setShowCreator] = useState(false);
-  const [obituaries, setObituaries] = useState(localStorage.obituaries ? JSON.parse(localStorage.obituaries) : []);
+  const [obituaries, setObituaries] = useState(
+    localStorage.obituaries ? JSON.parse(localStorage.obituaries) : []
+  );
 
   useEffect(() => {
     localStorage.setItem("obituaries", JSON.stringify(obituaries));
   }, [obituaries]);
 
+  const onAddObituary = async (image, name, birthDate, deathDate) => {
+    const data = new FormData();
+    data.append("image", image);
+    data.append("name", name);
+    data.append("birthDate", birthDate);
+    data.append("deathDate", deathDate);
 
-  const onAddObituary = (name, birthDate, deathDate) => {
-    const newObituary = {
-      name: name,
-      birthDate: birthDate,
-      deathDate: deathDate,
-      body: "Minion was known for his kind heart and adventurous spirit. He was always willing to lend a helping hand to anyone in need, and his infectious smile could light up a room. From his early days, Minion was drawn to exploration and discovery, and he dedicated his life to pushing the boundaries of what was possible. Minion's legacy will live on through the countless people whose lives he touched. Whether he was sharing stories of his travels or inspiring others to follow their dreams, Minion was a beacon of hope and inspiration to all who knew him.",
-      open: true,
-    };
-    setObituaries([...obituaries, newObituary]);
+    console.log(data);
+
+    try {
+      const res = await fetch(
+        "https://5woydzwm463jmh2ob3ovtjsc4i0nscof.lambda-url.ca-central-1.on.aws/",
+        {
+          method: "PUT",
+          body: data,
+        }
+      );
+
+      if (!res.ok) {
+        throw new Error(`An error occurred: ${res.statusText}`);
+      }
+
+      const newObituary = await res.json();
+
+      console.log(newObituary);
+
+      setObituaries((prevObituaries) => [...prevObituaries, newObituary]);
+    } catch (error) {
+      console.error("Error:", error);
+    }
   };
 
   const options = {
@@ -44,19 +66,32 @@ function App() {
 
   const onUpdateObituary = async (updatedObituary) => {
     const updatedObituaryArray = obituaries.map((obituary) => {
-        if (obituary.name === updatedObituary.name && obituary.birthDate === updatedObituary.birthDate && obituary.deathDate === updatedObituary.deathDate) {
+      if (
+        obituary.name === updatedObituary.name &&
+        obituary.birthDate === updatedObituary.birthDate &&
+        obituary.deathDate === updatedObituary.deathDate
+      ) {
         return updatedObituary;
-        }
-        return obituary;
-        });
+      }
+      return obituary;
+    });
     setObituaries(updatedObituaryArray);
-    };
+  };
 
   return (
     <div className="app-container">
-      <Header changeVisibilityCreator={changeVisibilityCreator}/>
-      <ObituaryList onUpdateObituary={onUpdateObituary} obituaries={obituaries} formatDate={formatDate}/>
-      {showCreator && <ObituaryCreator changeVisibilityCreator={changeVisibilityCreator} onAddObituary={onAddObituary}/>}
+      <Header changeVisibilityCreator={changeVisibilityCreator} />
+      <ObituaryList
+        onUpdateObituary={onUpdateObituary}
+        obituaries={obituaries}
+        formatDate={formatDate}
+      />
+      {showCreator && (
+        <ObituaryCreator
+          changeVisibilityCreator={changeVisibilityCreator}
+          onAddObituary={onAddObituary}
+        />
+      )}
     </div>
   );
 }
--- src/components/ObituaryCreator.js
+++ src/components/ObituaryCreator.js
@@ -1,41 +1,80 @@
 import { useState } from "react";
 
-function ObituaryCreator({changeVisibilityCreator, onAddObituary}) {
-    const [name, setName] = useState("");
-    const dt = new Date();
-    dt.setMinutes(dt.getMinutes() - dt.getTimezoneOffset());
-    const [birthDate, setBirthDate] = useState();
-    const [deathDate, setDeathDate] = useState();
-    const [img, setImg] = useState(null);
+function ObituaryCreator({ changeVisibilityCreator, onAddObituary }) {
+  const dt = new Date();
+  dt.setMinutes(dt.getMinutes() - dt.getTimezoneOffset());
+  const [name, setName] = useState("");
+  const [birthDate, setBirthDate] = useState("");
+  const [deathDate, setDeathDate] = useState("");
+  const [img, setImg] = useState(null);
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-        WriteObituary(name, birthDate, deathDate);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    WriteObituary(img, name, birthDate, deathDate);
+  };
+
+  const WriteObituary = (img, name, birthDate, deathDate) => {
+    onAddObituary(img, name, birthDate, deathDate);
+    changeVisibilityCreator();
+  };
+
+  const handleImageChange = (e) => {
+    if (e.target.files.length > 0) {
+      setImg(e.target.files[0]);
     }
+  };
 
-    const WriteObituary = (name, birthDate, deathDate) => {
-        onAddObituary(name, birthDate, deathDate);
-        changeVisibilityCreator();
-    };
-
-    return (
-        <form className="obituary-creator-container" onSubmit={onSubmit}>
-            <div className="obituary-creator-close-button" onClick={changeVisibilityCreator}>x</div>
-            <h1 className="title">Create a New Obituary</h1>
-            <label htmlFor="img-upload" className="img-upload-label">
-                <input id="img-upload" type="file" onChange={(e) => setImg(e.target.files[0])}/>
-                {img ? (<p>Select an image for the deceased ({img.name})</p>) : (<p>Select an image for the deceased</p>)}
-            </label>
-            <input className="creator-name-input" type="text" placeholder="Name of the deceased" value={name} onChange={(e) => setName(e.target.value)} required/>
-            <div className="creator-date-container">
-                <p>Born: </p>
-                <input className="date-input-birth" type="datetime-local" onChange={(e) => setBirthDate(e.target.value)} required/>
-                <p>Died: </p>
-                <input className="date-input-death" type="datetime-local" onChange={(e) => setDeathDate(e.target.value)} required/>
-            </div>
-            <button type="submit" value="Submit" className="creator-submit-button" >Create Obituary</button>
-        </form>
-    );
+  return (
+    <form className="obituary-creator-container" onSubmit={onSubmit}>
+      <div
+        className="obituary-creator-close-button"
+        onClick={changeVisibilityCreator}
+      >
+        x
+      </div>
+      <h1 className="title">Create a New Obituary</h1>
+      <label htmlFor="img-upload" className="img-upload-label">
+        <input
+          id="img-upload"
+          type="file"
+          required
+          onChange={handleImageChange}
+        />
+        {img ? (
+          <p>Select an image for the deceased ({img.name})</p>
+        ) : (
+          <p>Select an image for the deceased</p>
+        )}
+      </label>
+      <input
+        className="creator-name-input"
+        type="text"
+        placeholder="Name of the deceased"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        required
+      />
+      <div className="creator-date-container">
+        <p>Born: </p>
+        <input
+          className="date-input-birth"
+          type="datetime-local"
+          onChange={(e) => setBirthDate(e.target.value)}
+          required
+        />
+        <p>Died: </p>
+        <input
+          className="date-input-death"
+          type="datetime-local"
+          onChange={(e) => setDeathDate(e.target.value)}
+          required
+        />
+      </div>
+      <button type="submit" value="Submit" className="creator-submit-button">
+        Create Obituary
+      </button>
+    </form>
+  );
 }
 
 export default ObituaryCreator;
